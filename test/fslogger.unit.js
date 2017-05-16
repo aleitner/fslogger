769dcf7f7e1e754b75b3ee9c39617cb9e6a28090
Alexander Leitner <leitnersalex@gmail.com>
2017-05-16 15:14:39 -0400

    Added initial test

--- test/fslogger.unit.js
+++ test/fslogger.unit.js
@@ -6,13 +6,25 @@ var utils = require('../lib/utils');
 var proxyquire = require('proxyquire');
 var sinon = require('sinon');
 var os = require('os');
+var tmpdir = os.tmpdir();
+var FsLogger = require('./../lib/fslogger');
+var fs = require('fs');
 
 describe('fslogger', function() {
 
   describe('@constructor', function() {
-    it('it will set default log level to 3', function() {
 
+    var fslogger = new FsLogger(tmpdir, 'butts');
+
+    it('it will set default log level to 3', function() {
+      expect(fslogger._loglevel).to.equal(3);
     });
+
+    it('it will set the prefix', function() {
+      expect(fslogger._prefix).to.equal('butts_');
+    });
+
+    fs.unlinkSync(fslogger._logfile);
   });
 
 });
